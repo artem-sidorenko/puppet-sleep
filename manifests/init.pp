bea9fecc628458606274e9f75847da41a6f86b51
Artem Sidorenko <artem@2realities.com>
2013-06-28 19:14:17 +0200

    fixed after puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,20 @@
-class sleep( $period, $ports, $suspend_command ) {
+#puppet-sleep
+#Author: Artem Sidorenko <artem@2realities.com>
+class sleep(
+  $period=15,
+  $ports='22,80,443',
+  $suspend_command='systemctl suspend'
+  ) {
 
-  file { "/usr/local/libexec/sleep":
-    content => template("modules/sleep.erb"),
+  file { '/usr/local/libexec/sleep':
+    content => template('modules/sleep.erb'),
     mode    => '0755',
   }
-  cron { "sleep":
-    command => "/usr/local/libexec/sleep",
+  cron { 'sleep':
+    command => '/usr/local/libexec/sleep',
     user    => root,
     minute  => $period,
-    require => File["/usr/local/libexec/sleep"],
+    require => File['/usr/local/libexec/sleep'],
   }
 
 }
