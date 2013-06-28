9d1d29dce07ca9ffb0ba18c567171c977d5f1821
Artem Sidorenko <artem@2realities.com>
2013-06-28 19:49:14 +0200

    added params and fixed formatting

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,46 @@
 class sleep(
   $period=15,
   $ports='22,80,443',
-  $suspend_command='systemctl suspend'
+  $suspend_command='UNSET'
   ) {
 
-  file { '/usr/local/libexec/sleep':
+  #some defaults
+  $script_name = 'sleep'
+  $template_name = "${module_name}/sleep.erb"
+
+  case $::osfamily {
+    Archlinux: {
+      $supported = true
+      $script_location = '/usr/local/libexec'
+      $packages_requiered = 'UNSET'
+      if ($suspend_command == 'UNSET') {
+        $suspend_command = 'systemctl suspend'
+      }
+    }
+
+      default: {
+        fail("The ${module_name} module is not supported on ${::osfamily} based systems")
+      }
+
+    }
+
+  if ( $packages_requiered != 'UNSET' ) {
+    package { 'puppet-sleep':
+      ensure => present,
+      name   => $packages_requiered,
+    }
+  }
+
+  file { "${module_name}-script":
+    path    => "${script_location}/${script_name}",
     content => template('modules/sleep.erb'),
     mode    => '0755',
   }
-  cron { 'sleep':
-    command => '/usr/local/libexec/sleep',
+  cron { "${module_name}-cron":
+    command => "${script_location}/${script_name}",
     user    => root,
     minute  => $period,
-    require => File['/usr/local/libexec/sleep'],
+    require => File["${module_name}-script"],
   }
 
 }
