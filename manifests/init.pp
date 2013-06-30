86b10420266a12e38e59b9a7d12f91f5cff0c3dc
Artem Sidorenko <artem@2realities.com>
2013-06-30 18:30:17 +0200

    fixed the indent

--- manifests/init.pp
+++ manifests/init.pp
@@ -66,11 +66,11 @@ class sleep(
       }
     }
 
-      default: {
-        fail("The ${module_name} module is not supported on ${::osfamily} based systems")
-      }
-
+    default: {
+      fail("The ${module_name} module is not supported on ${::osfamily} based systems")
     }
+
+  }
 
   if ( $packages_requiered != 'UNSET' ) {
     package { "${module_name}-package":
